e9b34c99f27881e2edf4427fb98b00f12871af94
karnwatcharasupat <karn001@e.ntu.edu.sg>
2021-05-17 02:55:00 -0400

    remove unnecessary stuff

--- MATLAB:Weka Timbral Reverb/src/Tools/RAA_sigmoid_scaling.m
+++
@@ -1,8 +0,0 @@
-function [ Sx ] = RAA_sigmoid_scaling( ax, bx, Px )
-%RAA_sigmoid_scaling calculates scaled parameter values of RAA output 
-%   based on sigmoid function
-
-Sx = 1 / (1+exp(-ax*(Px-bx)));
-
-end
-
